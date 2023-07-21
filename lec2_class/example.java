c3d29e0e0a8ef1d8bbf52c02e637fb7b5455ff12
seeker4 <resurgam4@outlook.com>
2023-07-21 16:29:54 +0800

    simple example for class definition

---
+++ lec2_class/example.java
@@ -0,0 +1,12 @@
+public class example{
+	/**
+	 * 
+	 * simple description for class definition in java 
+	 */
+	public static void main(String[] args){
+		System.out.println("this \"main\" function only called by java language intepreter");
+		Dog d = new Dog(20);
+        d.makeNoise();
+		System.out.println(args[0]);
+	}
+}
