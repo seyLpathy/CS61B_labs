24a9c274e62bed3326c167f157167e891b9310c0
seeker4 <resurgam4@outlook.com>
2023-07-25 15:10:10 +0800

    remove lec2_class

--- lec2_class/sumarg.java
+++
@@ -1,9 +0,0 @@
-public class sumarg{
-	public static void main(String[] args){
-		int answer = 0;
-		for (int i = 0; i<args.length;i++){
-			answer += toint(args[i]);
-		}
-		System.out.println("the sum of the args above is "+ answer);
-	}
-}
