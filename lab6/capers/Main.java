745afa61064d0f316dfd2972d26707131baaf914
seeker4 <resurgam4@outlook.com>
2023-08-24 16:53:14 +0800

    8/24 lab6

--- lab6/capers/Main.java
+++ lab6/capers/Main.java
@@ -1,6 +1,7 @@
 package capers;
 
 import java.io.File;
+import java.util.Arrays;
 
 import static capers.Utils.*;
 
@@ -53,6 +54,8 @@ public class Main {
         case "dog":
             validateNumArgs("dog", args, 4);
             // TODO: make a dog
+            int dogAge = Integer.parseInt(args[3]);
+            CapersRepository.makeDog(args[1],args[2],dogAge);
             break;
         case "birthday":
             validateNumArgs("birthday", args, 2);
