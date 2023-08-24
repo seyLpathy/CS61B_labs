eb2d9e94737cf5fa5a5e98dafc5c269faacd459a
BlueCyan <resurgam4@outlook.com>
2023-08-24 19:49:51 +0800

    updated for git pull

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,71 +1,79 @@
 package deque;
-
 public class ArrayDeque<item> {
 	private item[] items;
 	private int size;
 	private int nextLast;
 	private int nextFirst;
-	public ArrayDeque(){
+
+	public ArrayDeque() {
 		items = (item[]) new Object[8];
 		size = 0;
-		nextLast=5;
-		nextFirst=4;
+		nextLast = 5;
+		nextFirst = 4;
 	}
-	private void resize(int capacity){
+
+	private void resize(int capacity) {
 		item[] a = (item[]) new Object[capacity];
-		System.arraycopy(items,0,a,size,size);
+		System.arraycopy(items, 0, a, size, size);
 		items = a;
-		nextLast =size()*2 + 1;
-		nextFirst=size() -1;
+		nextLast = size() * 2;
+		nextFirst = size() - 1;
 	}
-	public int size(){return size;}
-	public void addLast(item x){
-		if (size==items.length){
-			resize(size*3);
+
+	public int size() {
+		return size;
+	}
+
+	public void addLast(item x) {
+		if (size == items.length) {
+			resize(size * 3);
 			//geometrical resizing
 		}
-		items[nextLast]=x;
+		items[nextLast] = x;
 		size = size + 1;
-		if(nextLast+1== items.length){
-			nextLast=0;
-		}
-		else{
-			nextLast=nextLast+1;
+		if (nextLast + 1 == items.length) {
+			nextLast = 0;
+		} else {
+			nextLast = nextLast + 1;
 		}
 
 	}
-	public void addFirst(item x){
-		if(size==items.length) {
+
+	public void addFirst(item x) {
+		if (size == items.length) {
 			resize(size * 3);
 		}
-		items[nextFirst]=x;
+		items[nextFirst] = x;
 		size = size + 1;
-		if (nextFirst==0){
-			nextFirst = items.length -1;
-		}
-		else{
-			nextFirst = nextFirst -1;
+		if (nextFirst == 0) {
+			nextFirst = items.length - 1;
+		} else {
+			nextFirst = nextFirst - 1;
 		}
 
 
 	}
-	public boolean isEmpty(){
-		return size==0;
+
+	public boolean isEmpty() {
+		return size == 0;
 	}
-	public void printDeque(){
-		for (int i = 0;i<items.length;i++){
-			if (items[i]!=null){
+
+	public void printDeque() {
+		System.out.println("current members are displayed below");
+		for (int i = 0; i < items.length; i++) {
+			if (items[i] != null) {
 				System.out.print(items[i]);
 				System.out.print(' ');
 			}
 		}
+		System.out.println();
 	}
-	public item get(int index){
-		if (index > size){
+
+	public item get(int index) {
+		if (index > size) {
 			return null;
-		}
-		else {
-			item a =null;
+		} else {
+			item a = null;
 			for (int j = 0; j <= items.length; j++) {
 				if (items[j] != null) {
 					a = items[j + index];
@@ -75,14 +83,38 @@ public class ArrayDeque<item> {
 			return a;
 		}
 	}
-	public item removeLast() {
+	private void usageCheck(){
 		if (items.length >= 16 && size < items.length / 4) {
 			resize(size * 4);
 		}
-		item x = get(nextLast-1);
-		size =size -1;
+	}
+	public item removeLast() {
+		usageCheck();
+        int last;
+        if(nextLast==0){
+            last = items.length -1;
+        }
+		else{
+			last = nextLast -1;
+		}
+		item x = get(last);
+		size = size - 1;
+		nextLast = last;
 		return x;
-//
-//	}
+	}
+	public item removeFirst(){
+		usageCheck();
+		int first;
+		if(nextFirst== items.length-1){
+			first = 0;
+		}
+		else{
+			first = nextFirst -1;
+		}
+		item x = get(first);
+		size = size - 1;
+		nextFirst = first;
+		return x;
+	}
 
 }
--- proj1/deque/ArrayDequeTest.java
+++ proj1/deque/ArrayDequeTest.java
@@ -9,9 +9,19 @@ public class ArrayDequeTest {
 		t1.addFirst(5);
 		t1.addLast(7);
 		t1.addLast(6);
+		t1.addFirst(23);
+		t1.addLast(34);
+		t1.addFirst(27);
+		t1.addLast(67);
 		t1.printDeque();
-		assertEquals(4,t1.size());
-		assertEquals(7,(int) t1.get(2));
+		assertEquals(8,t1.size());
+		assertEquals(27,(int) t1.get(2));
+		/** test about resizing problems*/
+		t1.addLast(100);
+		t1.printDeque();
+		assertEquals(27,(int)t1.removeFirst());
+
+
 
 	}
 }
